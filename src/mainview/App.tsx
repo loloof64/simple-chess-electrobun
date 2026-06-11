5757d519f131fb230471f42510504aa04100355a
laurent bernabé <laurent.bernabe@gmail.com>
2026-06-11 20:52:42 +0200

    feat: we can make legal moves

--- src/mainview/App.tsx
+++ src/mainview/App.tsx
@@ -1,9 +1,43 @@
-import { Chessboard } from 'react-chessboard';
-
-const chessboardOptions = {
-};
+import { Chess } from 'chess.js';
+import { useRef, useState } from 'react';
+import { Chessboard, PieceDropHandlerArgs } from 'react-chessboard';
 
 function App() {
+
+	const chessGameRef = useRef(new Chess());
+    const chessGame = chessGameRef.current;
+
+	const [chessPosition, setChessPosition] = useState(chessGame.fen());
+
+	function onPieceDrop({
+      sourceSquare,
+      targetSquare
+    }: PieceDropHandlerArgs) {
+      if (!targetSquare) {
+        return false;
+      }
+
+      try {
+        chessGame.move({
+          from: sourceSquare,
+          to: targetSquare,
+          promotion: 'q'
+        });
+
+        setChessPosition(chessGame.fen());
+
+        return true;
+      } catch {
+        return false;
+      }
+    }
+
+	const chessboardOptions = {
+      position: chessPosition,
+      onPieceDrop,
+      id: 'mainGame'
+    };
+
 
 	return (
 		<div className="h-full min-h-screen bg-gradient-to-br from-orange-500 to-red-900 flex flex-col md:flex-row">
