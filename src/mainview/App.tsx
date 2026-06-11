3ecb1d27cb18a501aee23c43dac304762194586b
laurent bernabé <laurent.bernabe@gmail.com>
2026-06-11 21:25:43 +0200

    feat: show turn color

--- src/mainview/App.tsx
+++ src/mainview/App.tsx
@@ -7,6 +7,10 @@ function App() {
 	const chessGameRef = useRef(new Chess("rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"));
     const chessGame = chessGameRef.current;
 
+	function getTurnColor() : string {
+		return chessGame.turn() === 'b' ? 'bg-black' : 'bg-white';
+	}
+
 	const [chessPosition, setChessPosition] = useState(chessGame.fen());
 	const [history, setHistory] = useState(`${getMoveNumberString()}`);
 
@@ -56,6 +60,7 @@ function App() {
 	return (
 		<div className="h-full min-h-screen bg-gradient-to-br from-orange-500 to-red-900 flex flex-col md:flex-row">
 			<div className='w-5/6 md:w-2/3 mx-auto md:my-auto md:h-3/4 my-4 md:mx-4'>
+				<div className={`w-10 h-10 ${getTurnColor()} m-2 rounded-full`}></div>
 				<Chessboard options={chessboardOptions} />
 			</div>
 			<div className='bg-white w-5/6 mx-auto my-4 md:mx-4 grow text-2xl p-4'>
